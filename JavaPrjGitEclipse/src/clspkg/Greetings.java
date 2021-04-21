f9cb9335a885d28af4d470d1d6049dc546338607
l00156770 <l00156770@ubuntu>
2021-04-21 16:29:53 -0700

    testing jenkins build in April 2021

--- JavaPrjGitEclipse/src/clspkg/Greetings.java
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -15,7 +15,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings LYIT Student L00156770");
-		System.out.println("This change should be captured by JENKINS");
+		System.out.println("This change should be captured by JENKINS here - APRIL 2021");
 	}
 
 }
